67946a8b8e433c5c0fb80ee356d91c4a4073393e
flor4 <salanson.flora@outlook.fr>
2026-01-16 15:12:53 +0100

    Add: an interface named Teacher

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+// Create teacher3
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
