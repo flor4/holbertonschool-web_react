468659ca234e7310ab896f31bbc378801109df70
flor4 <salanson.flora@outlook.fr>
2026-01-16 17:45:03 +0100

    Refactor: structure and better organized

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,14 +1,62 @@
+// ----- Interfaces -----
 // Teacher interface
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [propName: string]: any;
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+
+  [key: string]: any,
 }
 
-// Create teacher3
+// Director interface
+interface Director extends Teacher {
+  numberOfReports: number,
+}
+
+// printTeacher interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Student class interface
+interface StudentClassInterface {
+  workOnHomework(): string;
+
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+// ----- Classes -----
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// ----- Functions -----
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+// ----- Tests -----
+// Teacher test
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -17,19 +65,21 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
-
-// Directors interface that extends Teacher
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-// Create director1
-const director1: Directors = {
+// Director test
+const director1: Director = {
   firstName: 'John',
   lastName: 'Doe',
   location: 'London',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
+
+// Student test
+const student1 = new StudentClass('John', 'Doe');
+
+// ----- Logs -----
+console.log(teacher3);
 console.log(director1);
+console.log(printTeacher("John", "Doe"));
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
